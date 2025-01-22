246176bc1dbccf9ca5c0ffe9301198adba933417
Nipun Sud <nipunsud@macbookpro.lan>
2025-01-22 10:44:51 -0500

    fix copy

--- src/components/HeroSectionFast/HeroSectionFast.js
+++ src/components/HeroSectionFast/HeroSectionFast.js
@@ -9,13 +9,13 @@ const HeroSectionFast = () => {
   return (
     <div className="flex flex-col items-center mt-6 mx-auto container bg-custom-purple text-white pt-8 pb-8 rounded-lg px-4 sm:px-8">
       <h4 className="text-white font-black text-center max-w-xl xl:max-w-2xl text-balance mx-2.5 text-4xl sm:text-5xl lg:text-6xl">
-        Simplify Investment Research with AI Assistants
+        Simplify Investments
       </h4>
       <p className="mt-4 text-gray-200 font-light text-center max-w-2xl text-balance text-lg sm:text-xl lg:text-2xl">Level the playing field for individual investors.</p>
       <h3 className="font-bold text-balance text-center max-w-4xl text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl text-white">
-        Invest with confidence. Sign up today to experience {" "}
-        <span className="text-black">AI-driven investment research</span>
-        that puts you in control, unlocks new opportunities, and levels the playing field once and for all.
+        {" "}
+        <span className="text-black">AI-driven investment research </span>
+        at your fingertips.
       </h3>
       <button
         className="mt-6 px-6 py-3.5 bg-black text-white font-semibold rounded-lg text-lg hover:!bg-gray-950 hover:!bg-opacity-90 transition-colors duration-300"
