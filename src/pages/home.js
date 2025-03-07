0e5d52705e2774286f90342fd7da8e83666f0cfb
manuel <onwaxcomm@gmail.com>
2025-03-07 15:44:16 -0300

    cleaning the home component

--- src/pages/home.js
+++ src/pages/home.js
@@ -82,40 +82,7 @@ const Home = () => {
         <Benefits /> 
       </Section>
 
-      {/* <HeroSection />
-
-      <Section
-        name="Applications"
-        title="Agents that integrate with everyday tools to automate daily processes."
-        subtitle="Discover the agents that enhance your daily tasks."
-      >
-        <Applications /> 
-      </Section>
-
-      <Section 
-        name="Use cases" 
-        title="Many problems we can solve"
-        subtitle="Our agents are diversa and range from to y"
-      >
-        <UseCases/>
-      </Section>
-
-      <Section 
-        name="Technology" 
-        title="Our Innovative Solutions"
-        subtitle="Discover how our technology is shaping the future."
-      >
-        <Benefits /> 
-      </Section>
-
-      <Section 
-        name="Testimonials" 
-        title="People love us!"
-        subtitle="You are more than welcome to our family, come share some love."
-      >
-        <Testimonials /> 
-      </Section> */}
-
+  
       <Section
         name="Contact Us"
         title="Get in Touch"
